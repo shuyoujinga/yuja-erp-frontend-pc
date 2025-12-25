e5a1b777a558418d362758e8d64b52221a3cdfeb
舒有敬 <shuyoujing@gmail.com>
2025-12-25 12:10:40 +0800

    [生产工单]v1版本代码提交

--- src/views/prdworkorder/V20251225_1__menu_insert_PrdWorkOrder.sql
+++ src/views/prdworkorder/V20251225_1__menu_insert_PrdWorkOrder.sql
@@ -5,7 +5,7 @@
 INSERT INTO sys_permission(id, parent_id, name, url, component, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_route, is_leaf, keep_alive, hidden, hide_tab, description, status, del_flag, rule_flag, create_by, create_time, update_by, update_time, internal_or_external) 
 VALUES ('2025122511448750170', NULL, '生产工单', '/prdworkorder/prdWorkOrderList', 'prdworkorder/PrdWorkOrderList', NULL, NULL, 0, NULL, '1', 0.00, 0, NULL, 1, 0, 0, 0, 0, NULL, '1', 0, 0, 'admin', '2025-12-25 11:44:17', NULL, NULL, 0);
 
--- 权限控制sql
+-- 权限控制sql1
 -- 新增
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
 VALUES ('2025122511448750171', '2025122511448750170', '添加生产工单', NULL, NULL, 0, NULL, NULL, 2, 'prdworkorder:prd_work_order:add', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2025-12-25 11:44:17', NULL, NULL, 0, 0, '1', 0);
